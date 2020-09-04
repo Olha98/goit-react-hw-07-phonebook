07009c379fd11e28be2e18fd92d8800982fb5f7c
Olha Hrymchak <forwork1177@gmail.com>
2020-09-04 13:10:02 +0300

    added all

--- src/Redux/Selections/selections.js
+++ src/Redux/Selections/selections.js
@@ -11,6 +11,4 @@ const getFilterSelection = state => state.filter
 
 // const getContactFilterSelection = state => state.contacts.filter(contact => contact.name.toLowerCase().includes(state.filter.toLowerCase()))
 const getContactFilterSelection = ([getContactSelection], state => state.contacts.filter(contact => contact.name.toLowerCase().includes(state.filter.toLowerCase()))) 
-
-
 export default { getContactSelection, getAlertSelection, getFilterSelection, getContactFilterSelection }
