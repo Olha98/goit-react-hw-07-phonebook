9becb27cbd1c3a8de371e65ef4e8ef22a76dd9b2
Olha Hrymchak <forwork1177@gmail.com>
2020-09-07 15:40:30 +0300

    Updates

---
+++ precache-manifest.98f5cbb012faccbd00bcef965422d04a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d349c5688590cc1319a79dfa3e9fba",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "8488cc8dc0d71df098db",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.3288777a.chunk.css"
+  },
+  {
+    "revision": "b20f6560076ebcacf34e",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.bdacee17.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.bdacee17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8488cc8dc0d71df098db",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.94f93454.chunk.js"
+  },
+  {
+    "revision": "e3f60b8770ae85b5a663",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.6ec4936c.js"
+  }
+]);
